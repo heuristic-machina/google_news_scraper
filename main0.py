c7508601307ceda243d2874cc8c0a6145319befe
heuristic_machina <machine_learner@yahoo.com>
2025-01-17 01:49:42 -0700

    limit maximum articles to 1

--- main0.py
+++ main0.py
@@ -1,14 +1,14 @@
 import feedparser
 import sys
 
-def fetch_news(search_terms, max_articles):
-        for search_term in search_terms:
-            search_term = search_term.replace(" ", "+")
-            gn_url = f"https://news.google.com/rss/search?q={search_term}&hl=en-US&gl=US&ceid=US:en"
-            gn_feed = feedparser.parse(gn_url)
+def fetch_news(search_terms):
+    for search_term in search_terms:
+        search_term = search_term.replace(" ", "+")
+        gn_url = f"https://news.google.com/rss/search?q={search_term}&hl=en-US&gl=US&ceid=US:en"
+        gn_feed = feedparser.parse(gn_url)
 
-        if gn_feed.entries[:max_articles]:
-            for news_item in gn_feed.entries[:max_articles]:
+        if gn_feed.entries:
+            for news_item in gn_feed.entries:
                 news_title = news_item.title
                 news_link = news_item.link
                 publication_date = news_item.published
@@ -21,11 +21,10 @@ def fetch_news(search_terms, max_articles):
             print(f"No news found for the term: {search_term}")
 
 if __name__ == "__main__":
-    search_terms = sys.argv[1:-1]
-    max_articles = int(sys.argv[-1])
+    search_terms = sys.argv[1:]
     if not search_terms:
         print("No search terms provided. Exiting.")
         sys.exit()
-    fetch_news(search_terms, max_articles)
+    fetch_news(search_terms)
 
-# python main.py "meta llama 3.2" "chatgpt o1 model" 1
+# python main.py "meta llama 3.2" "chatgpt o1 model"
