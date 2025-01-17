54ad43cb143932ee8ed935d1e11fb3d5c75d11e8
heuristic_machina <machine_learner@yahoo.com>
2025-01-17 02:53:55 -0700

    export to csv

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import feedparser
 import sys
 import pyshorteners
+import csv
 
 # Shortens a given url using tinyURL
 def shorten_url(url):
@@ -16,7 +17,13 @@ def shorten_url(url):
 def clean_title(title):
      return title.rsplit(' - ', 1)[0].strip()
 
+
+# Fetches news articles based on search terms and writes them to csv file
 def fetch_news(search_terms, max_articles):
+    with open("news_data.csv", mode="w", newline="", encoding="utf-8-sig") as file:
+        writer = csv.writer(file)
+        # Write the CSV header
+        writer.writerow(["Headline", "URL", "Published", "Source", "Source URL"])
         for search_term in search_terms:
             search_term = search_term.replace(" ", "+")
             gn_url = f"https://news.google.com/rss/search?q={search_term}&hl=en-US&gl=US&ceid=US:en"
@@ -29,11 +36,24 @@ def fetch_news(search_terms, max_articles):
                 publication_date = news_item.published
                 news_source = news_item.source.get("title")
                 source_url = news_item.source.get("href")
-                print(
-                    f"Headline: {news_title}\nURL: {news_link}\nPublished: {publication_date}\nSource: {news_source} ({source_url})"
+                # Write each news item to the CSV file
+                writer.writerow(
+                    [
+                        news_title,
+                        news_link,
+                        publication_date,
+                        news_source,
+                        source_url
+                    ]
                 )
-                print(20 * "-")
-            # print(f"No news found for the term: {search_term}")
+        else:
+            print(f"No news found for the term: {search_term}")
+
+                # print(
+                #     f"Headline: {news_title}\nURL: {news_link}\nPublished: {publication_date}\nSource: {news_source} ({source_url})"
+                # )
+                # print(20 * "-")
+
 
 if __name__ == "__main__":
     search_terms = sys.argv[1:-1]
